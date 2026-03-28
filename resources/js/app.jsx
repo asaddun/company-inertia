c9b64a2a27db88949791e30f6e8f1e22a9e88ff4
Asad <muhammadasaddun@gmail.com>
2026-03-28 14:50:13 +0700

    Add default layout

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,10 +1,17 @@
 import { createInertiaApp } from "@inertiajs/react";
 import { createRoot } from "react-dom/client";
+import "../css/app.css";
+import MainLayout from "./Layouts/MainLayout";
 
 createInertiaApp({
     resolve: (name) => {
         const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
-        return pages[`./Pages/${name}.jsx`];
+        const page = pages[`./Pages/${name}.jsx`];
+
+        page.default.layout =
+            page.default.layout || ((page) => <MainLayout children={page} />);
+
+        return page;
     },
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />);
