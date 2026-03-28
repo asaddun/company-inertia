5972c61f844348724c0b269962ea19e6cd8e3bd3
Asad <muhammadasaddun@gmail.com>
2026-03-28 22:35:39 +0700

    Assign OfficeLayout to office pages

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,18 +2,30 @@ import { createInertiaApp } from "@inertiajs/react";
 import { createRoot } from "react-dom/client";
 import "../css/app.css";
 import MainLayout from "./Layouts/MainLayout";
+import OfficeLayout from "./Layouts/OfficeLayout";
 
 createInertiaApp({
     resolve: (name) => {
         const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
         const page = pages[`./Pages/${name}.jsx`];
 
-        page.default.layout =
-            page.default.layout || ((page) => <MainLayout children={page} />);
-
+        if (name.startsWith("Office/")) {
+            page.default.layout =
+                page.default.layout ||
+                ((page) => (
+                    <MainLayout>
+                        <OfficeLayout>{page}</OfficeLayout>
+                    </MainLayout>
+                ));
+        } else {
+            page.default.layout =
+                page.default.layout ||
+                ((page) => <MainLayout>{page}</MainLayout>);
+        }
         return page;
     },
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />);
     },
+    title: (title) => (title ? `${title} | Company` : "Company"),
 });
