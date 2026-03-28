89645c30991a731f5843727facd2b6bb566dd094
Asad <muhammadasaddun@gmail.com>
2026-03-28 14:51:26 +0700

    Change to recommended Inertia

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,13 +2,15 @@
 <html>
 
 <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     @viteReactRefresh
     @vite('resources/js/app.jsx')
-    @inertiaHead
+    <x-inertia::head />
 </head>
 
 <body>
-    @inertia
+    <x-inertia::app />
 </body>
 
 </html>
