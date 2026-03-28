2be8f2c556a4b882460475b6709b832da632933b
Asad <muhammadasaddun@gmail.com>
2026-03-28 22:36:17 +0700

    Add office dashboard (dev)

---
+++ resources/js/Pages/Office/Dashboard.jsx
@@ -0,0 +1,12 @@
+import { Head } from "@inertiajs/react";
+
+function Dashboard() {
+    return (
+        <>
+            <Head title="Dashboard" />
+            <div>Dashboard</div>
+        </>
+    );
+}
+
+export default Dashboard;
