518777cc924cd57a6ce25ceea69d109ba319c571
Asad <muhammadasaddun@gmail.com>
2026-05-14 21:23:47 +0700

    fix: limit global data for auth user

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,7 +37,7 @@ class HandleInertiaRequests extends Middleware
     {
         return array_merge(parent::share($request), [
             'auth' => [
-                'user' => $request->user(),
+                'user' => $request->user()?->only('id', 'name', 'username', 'level', 'bank_account_number', 'phone', 'identity_number'),
             ],
             'flash' => [
                 'success' => fn() => $request->session()->get('success'),
